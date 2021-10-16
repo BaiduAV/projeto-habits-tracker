2cafcdc468d38bf1cb03a9c46a54c0c4b17c719e
Luiz Victor <luizvictornacimento00@gmail.com>
2021-10-16 00:13:58 -0300

    fix: wrong pathway

--- src/pages/groupCreation/index.jsx
+++ src/pages/groupCreation/index.jsx
@@ -1,7 +1,9 @@
+import CreateGroup from "../../components/CreateGroup";
+
 export const GroupCreation = () => {
 	return (
 		<>
-			<div>Create a Group</div>
+			<div><CreateGroup /></div>
 		</>
 	)
 }
